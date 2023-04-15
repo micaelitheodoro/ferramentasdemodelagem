e0edeb7dd1f43146062aed7b6b4036607e6681e3
thomasvilches <thomvilches@gmail.com>
2023-04-15 09:50:05 -0300

    aula de 13/04

--- R/usando_dplyr.R
+++ R/usando_dplyr.R
@@ -15,8 +15,7 @@ library(stringr)
 #? https://www.kaggle.com/datasets/igorcoelho24/pokemon-all-generations/versions/1?resource=download
 dados <- read.csv("Dados/Pokemon_full.csv")
 head(dados) #? vê as primeiras linhas de dados
-
-
+#dados <- read.csv("D:/Aulas/ferramentasdemodelagem/R/Dados/Pokemon_full.csv")
 #? A biblioteca dplyr possui o operador "pipe"
 #? dado por  %>%
 #? Ele "pega" tudo que está à esquerda dele e coloca como primeiro elemento
@@ -24,24 +23,37 @@ head(dados) #? vê as primeiras linhas de dados
 #? Também é possível usar o operador "."
 #? para especificar onde ele deve substituir.
 
-
 #? Exemplo: contar o número de linhas de dados
 
 nrow(dados)
 dados %>% nrow()
 dados %>% nrow(.)
 
+
+## grepl() # verifica se um padrão de string esta em um elemento
+grepl("ap", "apple")
+grepl("apple", "ap")
+
+x <- "apple"
+
+x %>% grepl("ap", .)
+x %>% grepl("ap")
+
 #? Algumas funções da biblioteca dplyr
 
 #? A função filter seleciona linhas com base em um teste
-filter(dados, type == "grass")
+df_grass <- filter(dados, type == "grass")
+df_grass
 
 #? podemos usar o seguinte comando também
-dados %>% filter(type == "grass")
+ dados %>% filter(type == "grass")
 
 #TODO Vamos filtrar todos os pokemons do tipo fogo ou água
 
+df_fogo_e_agua <- dados %>% filter(type == "fire" | type == "water")
+
 #TODO Vamos filtrar todos os pokemons que tem  "fly"
+dados %>% filter(grepl("fly", name))
 
 #TODO Vamos filtrar todos os pokemons que tem  "bee" ou "saur"
 
